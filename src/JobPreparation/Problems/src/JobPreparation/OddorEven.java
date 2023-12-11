7f6c491b590cde8b8a992c3d746732e23c0a2533
Meena <meena27072001@gmail.com>
2023-12-11 17:29:49 +0530

    OddorEven.java

---
+++ src/JobPreparation/Problems/src/JobPreparation/OddorEven.java
@@ -0,0 +1,11 @@
+package JobPreparation;
+
+public class OddorEven {
+	public static void main(String arg[]) {
+		int i=4;
+		String s=(i%2==0)? "even":"odd";
+		System.out.println(s);
+				
+	}
+
+}
